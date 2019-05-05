4da3819eddbaf5b74ec44d5f0bbb240d057fe087
Andy Stewart <lazycat.manatee@gmail.com>
2019-05-05 17:25:11 +0800

    first commit

---
+++ lazy-load.el
@@ -0,0 +1,126 @@
+;;; lazy-load.el --- Lazy load keys for speed ​​up Emacs startup.
+
+;; Filename: lazy-load.el
+;; Description: Lazy set keystroke.
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2009, Andy Stewart, all rights reserved.
+;; Created: 2009-02-07 22:32:50
+;; Version: 0.1
+;; Last-Updated: 2009-02-07 22:32:50
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/lazy-load.el
+;; Keywords:
+;; Compatibility: GNU Emacs 23.0.60.1
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Lazy set keystroke.
+;;
+
+;;; Installation:
+;;
+;; Put lazy-load.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'lazy-load)
+;;
+;; No need more.
+
+;;; Change log:
+;;
+;; 2019/05/05
+;;      * Refactory code.
+;;
+;; 2009/02/07
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+
+
+;;; Code:
+
+(defun lazy-load-global-keys (key-alist filename)
+  (lazy-load-set-keys key-alist)
+  (dolist (element key-alist)
+    (setq fun (cdr element))
+    (autoload fun filename)))
+
+(defun lazy-load-local-keys (key-alist keymap key-prefix filename)
+  (lazy-load-set-keys key-alist keymap key-prefix)
+  (dolist (element key-alist)
+    (setq fun (cdr element))
+    (autoload fun filename)))
+
+(defun lazy-load-set-keys (key-alist &optional keymap key-prefix)
+  "This function is to little type when define key binding.
+`KEYMAP' is a add keymap for some binding, default is `current-global-map'.
+`KEY-ALIST' is a alist contain main-key and command.
+`KEY-PREFIX' is a add prefix for some binding, default is nil."
+  (let (key def)
+    (or keymap (setq keymap (current-global-map)))
+    (if key-prefix
+        (setq key-prefix (concat key-prefix " "))
+      (setq key-prefix ""))
+    (dolist (element key-alist)
+      (setq key (car element))
+      (setq def (cdr element))
+      (cond ((stringp key) (setq key (read-kbd-macro (concat key-prefix key))))
+            ((vectorp key) nil)
+            (t (signal 'wrong-type-argument (list 'array key))))
+      (define-key keymap key def))))
+
+(defun lazy-load-unset-keys (key-list &optional keymap)
+  "This function is to little type when unset key binding.
+`KEYMAP' is add keymap for some binding, default is `current-global-map'
+`KEY-LIST' is list contain key."
+  (let (key)
+    (or keymap (setq keymap (current-global-map)))
+    (dolist (key key-list)
+      (cond ((stringp key) (setq key (read-kbd-macro (concat key))))
+            ((vectorp key) nil)
+            (t (signal 'wrong-type-argument (list 'array key))))
+      (define-key keymap key nil))))
+
+(provide 'lazy-load)
+
+;;; lazy-load.el ends here
